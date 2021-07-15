a36e9f3e54530bfcddc775e05d94185769b853db
ehsan gn <ehsangn@protonmail.ch>
2021-07-15 18:35:53 +0430

    no need for slock and dmenu templates now

--- pywal/.config/wal/templates/dmenu.h
+++
@@ -1,9 +0,0 @@
-#define PYWAL
-static const char *colors[SchemeLast][2] = {{
-    /*                fg         bg      */
-    [SchemeNorm] = {{ "{color2}", "{background}" }},
-    [SchemeSel]  = {{ "{color14}", "{background}" }},
-    [SchemeSelHighlight]  = {{ "{color14}", "{background}" }},
-    [SchemeNormHighlight] = {{ "{color2}", "{background}" }},
-    [SchemeOut] = {{ "{color2}", "{background}" }},
-}};
--- pywal/.config/wal/templates/slock.h
+++
@@ -1,7 +0,0 @@
-#define PYWAL
-static const char * text_color = "{foreground}";
-static const char *colorname[NUMCOLS] = {{
-    [INIT] =   "{background}",
-    [INPUT] =  "{color2}",
-    [FAILED] = "{background}",
-}};
