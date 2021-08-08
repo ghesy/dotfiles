24f3795add25859a0d99a3fbbda24ec04ae8c642
Ehsan G <ehsangn@protonmail.ch>
2021-08-09 00:55:19 +0430

    finish 20-connectivity.conf.sysconf.sh

--- system-files/.local/share/system-files/NetworkManager/etc/NetworkManager/conf.d/20-connectivity.conf.sysconf.sh
+++ system-files/.local/share/system-files/NetworkManager/etc/NetworkManager/conf.d/20-connectivity.conf.sysconf.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 file="$(grep -l connectivity /usr/lib/NetworkManager/conf.d/*)" || exit 1
 file="$(echo "$file" | head -n1)"
-grep 'uri=' "$file" || exit 1
-awk '/uri=/ { print; print "interval=30"; next } 1' "$file" || exit 1
+grep -q 'uri=' "$file" || exit 1
+awk '/uri=/ { print; print "interval=30"; next } 1' "$file" \
+    > /etc/NetworkManager/conf.d/"$(basename "$file")" || exit 1
