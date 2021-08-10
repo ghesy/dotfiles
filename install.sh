d88a7b5ef5e7e42cd04d16eab03aff5a0c2ecd60
Ehsan G <ehsangn@protonmail.ch>
2021-08-11 01:43:43 +0430

    simplify the install script

--- install.sh
+++ install.sh
@@ -1,21 +1,12 @@
 #!/bin/sh
-# this script installs the dotfiles.
 
 # check if gnu stow is installed
-! which stow >/dev/null && echo install stow && exit 1
+! command -v stow >/dev/null && echo Please install stow. && exit 1
 
-# make directories that shouldn't be folded. if we don't create
-# them now, stow will create them as symlinks and any other file
-# that goes in there, will end up amongst our dotfiles, which is not ok.
-# read more on folding in stow's manpage.
-mkdir -p ~/.local/bin ~/.local/share/applications \
-    ~/.local/share/gnupg ~/.local/sv/run ~/.config/tremc ~/.config/kicad \
-    ~/.config/safeeyes ~/.config/pulse ~/.config/gtk-2.0 ~/.config/gtk-3.0 \
-    ~/.config/transmission-daemon
+# make these directories so that stow can't create them as symlinks
+l=~/.local; s=$l/share c=~/.config
+mkdir -p $l/bin $l/sv/run $s/applications $s/gnupg $c/tremc $c/kicad \
+    $c/safeeyes $c/pulse $c/gtk-2.0 $c/gtk-3.0 $c/transmission-daemon
 
-# install all the dotfile packages one by one.
-# if we try to install them all at once (by running 'stow */'),
-# stow will abort the whole thing on any error, and that's annoying.
-for pkg in */; do
-    stow -v "$pkg"
-done
+# install the dotfiles
+stow -v */
