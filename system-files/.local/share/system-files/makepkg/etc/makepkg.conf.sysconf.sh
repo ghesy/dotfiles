1b68897c7d377a7efe03c4603d7c243e00ece1e1
Ehsan G <ehsangn@protonmail.ch>
2021-08-11 01:02:02 +0430

    change the way makepkg.conf is configured

--- system-files/.local/share/system-files/makepkg/etc/makepkg.conf.sysconf.sh
+++ system-files/.local/share/system-files/makepkg/etc/makepkg.conf.sysconf.sh
@@ -1,37 +1,6 @@
 #!/bin/sh
-
 conf=/etc/makepkg.conf
-
-main() {
-    sum1=$(sha1sum $conf)
-    grep -q aria2c $conf || dlagents
-    grep -q nproc $conf || makeflags
-    grep -q 'ZST.*0' $conf || zstdflags
-    sum2=$(sha1sum $conf)
-    [ "$sum1" != "$sum2" ] && echo Updated $conf.
-}
-
-dlagents() {
-cat << 'eof' >> $conf
-DLAGENTS=('file::/usr/bin/aria2c --no-conf --min-split-size=1M --summary-interval=0 --max-connection-per-server=2 -o %o %u'
-          'ftp::/usr/bin/aria2c --ftp-pasv --no-conf --min-split-size=1M --summary-interval=0 --max-connection-per-server=2 -o %o %u'
-          'http::/usr/bin/aria2c --no-conf --min-split-size=1M --summary-interval=0 --max-connection-per-server=2 -o %o %u'
-          'https::/usr/bin/aria2c --no-conf --min-split-size=1M --summary-interval=0 --max-connection-per-server=2 -o %o %u'
-          'rsync::/usr/bin/rsync --no-motd -z %u %o'
-          'scp::/usr/bin/scp -C %u %o')
-eof
-}
-
-makeflags() {
-cat << 'eof' >> $conf
-MAKEFLAGS=-j$(nproc)
-eof
-}
-
-zstdflags() {
-cat << 'eof' >> $conf
-COMPRESSZST=(zstd -c -z -q -T0 -)
-eof
-}
-
-main "$@"
+[ ! -e $conf ] && exit 1
+grep -q '\. /etc/makepkg-extra.conf' $conf && exit
+printf '\n%s\n%s\n' '# include custom configs' '. /etc/makepkg-extra.conf' >> $conf
+echo Updated $conf.
