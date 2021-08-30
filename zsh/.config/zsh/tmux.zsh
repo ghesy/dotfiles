32ed22730e72e56a7eb9a2284c539821953f1c17
Ehsan G <ehsangn@protonmail.ch>
2021-08-30 22:14:21 +0430

    handle ssh connections in tmux.zsh

--- zsh/.config/zsh/tmux.zsh
+++ zsh/.config/zsh/tmux.zsh
@@ -1,15 +1,13 @@
 # vim:ft=zsh
 # start a new tmux session and window per terminal window,
 # and kill the session and window when the terminal window closes.
-[ -z "$TMUX" ] && [ -n "$DISPLAY" ] &&
+[ -n "$TMUX" ] || [[ $- != *i* ]] && return
+[ -n "$SSH_CONNECTION" ] && exec tmux new -Asssh
+[ -n "$DISPLAY" ] &&
 {
-    rand() { tr -dc 'a-z0-9' </dev/urandom | head -c20 }
-    tsesh=$(rand)
-    twndw=$(rand)
-
-    kill() { tmux kill-session -t $1 \; killw -t $2 }
-    trap "kill $tsesh $twndw" EXIT
-
+    tsesh=$$$RANDOM
+    twndw=$$$RANDOM
+    trap "tmux kill-session -t $tsesh \; killw -t $twndw" EXIT
     if [ "$(tmux ls 2>/dev/null | wc -l)" -lt 1 ]; then
         tmux new -s $tsesh -t main \; renamew $twndw
     else
