45293cb0ff1e3b0155dba04b7d0192237e93289c
Ehsan G <ehsangn@protonmail.ch>
2021-08-31 16:36:44 +0430

    move tmux.conf to zsh's dir

--- zsh/.config/zsh/tmux.zsh
+++ zsh/.config/zsh/tmux.zsh
@@ -1,16 +1,21 @@
-# vim:ft=zsh
-# start a new tmux session and window per terminal window,
-# and kill the session and window when the terminal window closes.
-[ -n "$TMUX" ] || [[ $- != *i* ]] && return
+# return if already in tmux or the shell isn't interactive
+[ -n "$TMUX" ] || [[ $- != *i* ]] || ! which tmux >/dev/null && return
+
+# else, start a vanilla tmux session if it's an ssh connection
 [ -n "$SSH_CONNECTION" ] && exec tmux new -Asssh
-[ -n "$DISPLAY" ] &&
-{
+
+# else, start a new tmux session and window per terminal window,
+# and kill the session and window when the terminal window closes.
+# this practically eliminates tmux's session management,
+# so we can only use it's scollback buffer without any intrusion.
+[ -n "$DISPLAY" ] && {
+    conf=~/.config/zsh/tmux.conf
     tsesh=$$$RANDOM
     twndw=$$$RANDOM
     trap "tmux kill-session -t $tsesh \; killw -t $twndw" EXIT
     if [ "$(tmux ls 2>/dev/null | wc -l)" -lt 1 ]; then
-        tmux new -s $tsesh -t main \; renamew $twndw
+        tmux -f $conf new -s $tsesh -t main \; renamew $twndw
     else
-        tmux new -s $tsesh -t main \; neww -n $twndw
+        tmux -f $conf new -s $tsesh -t main \; neww -n $twndw
     fi
 }
