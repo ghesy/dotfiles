981aeefc08ca46a397f280859bb67092c29f7d9c
Ehsan G <ehsangn@protonmail.ch>
2021-09-03 15:27:52 +0430

    use a unified directory for local configurations

--- install.sh
+++ install.sh
@@ -6,7 +6,8 @@
 # make these directories so that stow can't create them as symlinks
 l=~/.local; s=$l/share c=~/.config
 mkdir -p $l/bin $l/sv/run $s/applications $s/gnupg $c/kicad \
-    $c/pulse $c/gtk-2.0 $c/gtk-3.0 $c/transmission-daemon
+    $c/pulse $c/gtk-2.0 $c/gtk-3.0 $c/transmission-daemon \
+    $s/local/profile.d $s/local/xinitrc.d $s/local/gimme/aliases.d
 
 # install the dotfiles
 stow -v */
