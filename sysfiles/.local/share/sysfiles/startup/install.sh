9a27d42f348232af4279f9e5759eae10f7854e26
Ehsan G <ehsangn@protonmail.ch>
2021-09-16 18:36:12 +0430

    rc.local should be executable

--- sysfiles/.local/share/sysfiles/startup/install.sh
+++ sysfiles/.local/share/sysfiles/startup/install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # this script installs an rc.local file to do some stuff on startup.
 command -v ntpd >/dev/null || paru -S --needed ntp || exit 1
-install -DCvm644 rc.local /etc/rc.local
+install -DCvm755 rc.local /etc/rc.local
