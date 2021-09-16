6e7a6410b3ff43dd4d181e09f8d99a256a1f5a25
Ehsan G <ehsangn@protonmail.ch>
2021-09-16 20:08:54 +0430

    fix bug in sudo sysfiles

--- sysfiles/.local/share/sysfiles/sudo/install.sh
+++ sysfiles/.local/share/sysfiles/sudo/install.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 # install sudo-related configs
 install -DCvm644 faillock.conf /etc/security/faillock.conf
-install -DCvm644 config.sudo programs.sudo -t /etc/sudoers.d
+install -DCvm644 config.sudo /etc/sudoers.d/config
+install -DCvm644 programs.sudo /etc/sudoers.d/programs
