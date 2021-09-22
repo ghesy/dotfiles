3b9d6222a7f5c60415f2d0e28f076ded4dbc7093
Ehsan G <ehsangn@protonmail.ch>
2021-09-22 15:07:14 +0330

    add missing package installations in sysfiles

--- sysfiles/.local/share/sysfiles/makepkg/install.sh
+++ sysfiles/.local/share/sysfiles/makepkg/install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # this script configures makepkg to use aria2 for downloading files.
+command -v aria2c >/dev/null || pacman -S --needed aria2 || exit 1
 install -DCvm644 makepkg.extra.conf /etc/makepkg.extra.conf
 f=/etc/makepkg.conf
 [ -e $f ] && ! fgrep -q '. /etc/makepkg.extra.conf' $f &&
--- sysfiles/.local/share/sysfiles/powersaving/install.sh
+++ sysfiles/.local/share/sysfiles/powersaving/install.sh
@@ -4,6 +4,11 @@ case $(cat /sys/class/dmi/id/chassis_type) in
     9|10|11|14) ;;
     *) exit ;;
 esac
+
+# install the required packages
+pkg() { pacman -Q "$@" >/dev/null 2>&1 || pacman -S --needed "$@" || exit 1 ;}
+pkg hdparm light
+
 install -DCvm644 udev/* -t /etc/udev/rules.d
 install -DCvm644 sysctl.d/* -t /etc/sysctl.d
 install -DCvm644 modprobe.d/* -t /etc/modprobe.d
