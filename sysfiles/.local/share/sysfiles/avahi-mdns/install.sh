817b8d12974f912e111b57d64a1da2d60dc69b99
Ehsan G <ehsangn@protonmail.ch>
2021-09-26 21:12:39 +0330

    enable services after installing the files in sysfiles

--- sysfiles/.local/share/sysfiles/avahi-mdns/install.sh
+++ sysfiles/.local/share/sysfiles/avahi-mdns/install.sh
@@ -6,11 +6,11 @@
 pkg() { pacman -Q "$@" >/dev/null 2>&1 || pacman -S --needed "$@" || exit 1 ;}
 pkg nss-mdns avahi-runit
 
-# enable avahi's service
-ln -vs /etc/runit/sv/avahi-daemon /run/runit/service/ 2>/dev/null
-
 # add mdns4_minimal to /etc/nsswitch.conf
 f=/etc/nsswitch.conf
 grep -q 'hosts:.*mdns4' $f && exit
 sed -i '/hosts:/ s/resolve/mdns4_minimal [NOTFOUND=return] resolve/' $f &&
     echo Updated $f.
+
+# enable avahi's service
+ln -vs /etc/runit/sv/avahi-daemon /run/runit/service/ 2>/dev/null
--- sysfiles/.local/share/sysfiles/dns-caching/install.sh
+++ sysfiles/.local/share/sysfiles/dns-caching/install.sh
@@ -5,10 +5,10 @@
 pkg() { pacman -Q "$@" >/dev/null 2>&1 || pacman -S --needed "$@" || exit 1 ;}
 pkg openresolv unbound unbound-runit
 
-# enable unbound's service
-ln -vs /etc/runit/sv/unbound /run/runit/service/ 2>/dev/null
-
 install -DCvm644 dns.conf /etc/NetworkManager/conf.d/dns.conf
 install -DCvm644 unbound.conf /etc/unbound/unbound.conf
 install -DCvm644 resolvconf.conf /etc/resolvconf.conf
 install -DCvm755 update-resolv.sh /etc/windscribe/update-resolv.sh
+
+# enable unbound's service
+ln -vs /etc/runit/sv/unbound /run/runit/service/ 2>/dev/null
