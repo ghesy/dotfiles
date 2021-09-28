28e558d9e93b0a2bf37cd565eeef686f830fffe0
Ehsan G <ehsangn@protonmail.ch>
2021-09-28 12:03:44 +0330

    detect and use user's default group

--- sysfiles/.local/share/sysfiles/firefox/install.sh
+++ sysfiles/.local/share/sysfiles/firefox/install.sh
@@ -5,13 +5,14 @@
 pkg() { pacman -Q "$@" >/dev/null 2>&1 || paru -S --needed "$@" || exit 1 ;}
 pkg firefox python-pywalfox
 
-# get user's home directory
-HOME=$(getent passwd ${SUDO_USER:?} | cut -d: -f6)
+# get user's home directory and group
+HOME=$(getent passwd "${SUDO_USER:?}" | cut -d: -f6)
+group=$(id -gn "$SUDO_USER")
 
 # install the user.js file
-printf '%s\n' $HOME/.mozilla/firefox/*.*/ |
-    xargs -d'\n' -L1 install -DCvm644 -o $SUDO_USER -g nogroup user.js -t
+printf '%s\n' "$HOME"/.mozilla/firefox/*.*/ |
+    xargs -d'\n' -L1 install -DCvm644 -o "$SUDO_USER" -g "${group:-nobody}" user.js -t
 
 # install pywalfox
-[ ! -e ${HOME:?}/.mozilla/native-messaging-hosts/pywalfox.json ] &&
-    chmod -v 755 $(runuser -u $SUDO_USER -g nogroup pywalfox install | grep -Eom1 '\S+main.sh')
+[ ! -e "${HOME:?}"/.mozilla/native-messaging-hosts/pywalfox.json ] &&
+    chmod -v 755 $(runuser -u "$SUDO_USER" -g "${group:-nobody}" pywalfox install | grep -Eom1 '\S+main.sh')
