608b2a0a05b8d0098443251d7dfbf122915525cc
Ehsan G <ehsangn@protonmail.ch>
2021-09-28 21:08:32 +0330

    fix typo

--- sysfiles/.local/share/sysfiles/makepkg/install.sh
+++ sysfiles/.local/share/sysfiles/makepkg/install.sh
@@ -2,7 +2,7 @@
 # this script configures makepkg to use aria2 for downloading files.
 command -v aria2c >/dev/null || pacman -S --needed aria2 || exit 1
 install -DCvm644 makepkg.extra.conf /etc/makepkg.extra.conf
-install -Dcvm755 aria2-makepkg /usr/local/bin/aria2-makepkg
+install -DCvm755 aria2-makepkg /usr/local/bin/aria2-makepkg
 f=/etc/makepkg.conf
 [ -e $f ] && ! fgrep -q '. /etc/makepkg.extra.conf' $f &&
     printf '\n%s\n%s\n' '# include custom configs' '. /etc/makepkg.extra.conf' >> $f &&
