89e2bec75aae9c4196af26bb7f24534b3cecd2a0
Ehsan G <ehsangn@protonmail.ch>
2021-10-14 23:10:13 +0330

    improve mpv's scripts

--- mpv/.config/mpv/description.sh
+++ mpv/.config/mpv/description.sh
@@ -1,13 +1,17 @@
-#!/bin/bash
+#!/bin/sh
 # This script shows the description and comments of an internet video.
-# requires youtube-dl and pipe-viewer.
-[ -f "${1:?}" ] && exit 1
-url=${1#ytdl://*}
-notify-send -u low mpv 'Fetching Description...'
-set -o pipefail
-description=$(youtube-dl -jq --no-playlist "$url" | jq -r '"Title: \(.title)","Channel: \(.channel)","\n===============\n= Description \n===============\n",.description,"\n===============\n= Comments \n==============="') || {
-    notify-send mpv 'Fetching Description Failed.'
-    exit 1
-}
-comments=$(pipe-viewer --no-interactive --no-use-colors  --comments="$url")
-exec $TERMINAL -e sh -c 'printf "%s\n" "$@" | { nvim || ${PAGER:-less} ;}' '' "$description" "$comments"
+# requires jq, youtube-dl, pipe-viewer.
+set -- "${1#ytdl://*}"
+{
+if [ -f "${1:?}" ]; then
+    echo Description:
+    mediainfo --Output=JSON "${1:?}" | jq -r '.media.track[0].Description' | sed 's/ \/ /\n/g' | fold -sw70
+    set -- "$(mediainfo --Output=JSON "${1:?}" | jq -r '.media.track[0].extra.PURL')"
+else
+    echo Description:
+    p=$(getproxy "$1")
+    $p youtube-dl -jq "${1:?}" 2>/dev/null | jq -r .description
+fi
+printf '\nComments:\n'
+$p pipe-viewer --no-interactive --no-use-colors --comments="${1:?}"
+} 2>/dev/null | nvim -c 'setlocal nomodified nomodifiable'
--- mpv/.config/mpv/download.sh
+++ mpv/.config/mpv/download.sh
@@ -1,39 +1,22 @@
 #!/bin/sh
-# This script downloads the currently playing video.
-# requires aria2 and youtube-dl.
+# This script downloads the currently playing video using yt-dlp.
 
-# config
-aria2_args='-c -k1M -x16 -s16 --async-dns=false'
-
-# if the given argument is a file (therefore not a link), exit
 [ -f "${1:?}" ] && exit 1
-
-# go to download destination
 cd /media/downloads || cd ~/Downloads || cd ~ || exit 1
 
-# ask the desired quality via dmenu
 quality=$(printf '480p\n720p\n1080p\n360p\n' | dmenu -p Choose\ Quality ${WINDOWID:+-w $WINDOWID}) || exit 1
 quality=${quality%p}
 quality="bestvideo[height=${quality:?}]+bestaudio/best[height=$quality]/bestvideo[height<=$((quality*2))]+bestaudio/best[height<=$((quality*2))]/bestvideo+bestaudio/best"
 
-ytdl() {
-    ytdlp_args='-N 8 --compat-options no-keep-subs'
-    ytdl=youtube-dl
-    if command -v yt-dlp >/dev/null; then
-        ytdl=yt-dlp
-    elif [ ! -L $(command -v youtube-dl) ]; then
-        ytdlp_args=''
-    fi
-    $ytdl -f "$quality" --sub-lang=en --write-sub --write-auto-sub \
-        --embed-subs --external-downloader aria2c \
-        --external-downloader-args "$aria2_args" $ytdlp_args -- "$@"
-}
+yt-dlp -f "$quality" -N 8 --add-metadata --sub-lang=en \
+    --write-sub --write-auto-sub --embed-subs \
+    --compat-options no-keep-subs -- "${1#ytdl://*}"
 
-echo Downloading to "$PWD"...
-url=${1#ytdl://*}
-ytdl "$url" || aria2c $aria2_args -- "$url" &&
-    echo Download Finished. ||
-    echo Download Failed.
-echo Press Enter.
-read x
-exec $SHELL
+case $? in
+    0) echo Download finished.
+        read x
+        exec $SHELL ;;
+    *) echo Download failed.
+        read x
+        exit ;;
+esac
