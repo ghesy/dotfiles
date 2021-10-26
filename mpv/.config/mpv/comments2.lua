01236885d8244d74d3d79496cc05fe1d2087061e
Ehsan G <ehsangn@protonmail.ch>
2021-10-26 20:36:08 +0330

    comments2.lua: take it one step further

--- mpv/.config/mpv/comments2.lua
+++ mpv/.config/mpv/comments2.lua
@@ -1,6 +1,8 @@
 -- config
 COMMENTS_KEY = 'ctrl+c'
 DESCRIPTION_KEY = 'ctrl+d'
+COMMENTS_WRAP = 75
+COMMENTS_PREFIX = "│"
 ---
 
 local mp = require 'mp'
@@ -29,6 +31,44 @@ local function exec(args)
     return r.status, r.stdout
 end
 
+-- separate the given string into lines.
+function getlines(s)
+    if s:sub(-1)~="\n" then s=s.."\n" end
+    return s:gmatch("(.-)\n")
+end
+
+-- get the number of bytes that contain n utf8 characters from the string s.
+function uchar(s, n)
+    return utf8.offset(s, n+1)-1
+end
+
+-- wrap lines of the given string to WRAP columns.
+function wrap(s)
+    local w = {}
+    for line in getlines(s) do
+        local len = utf8.len(line)
+        if len == nil then len = #line end
+        if len <= WRAP then
+            w[#w + 1] = line
+        else
+            -- find the position of the first space character before the WRAPth column
+            local brk = string.find(string.reverse(string.sub(line, 1, uchar(line, WRAP))), "%s")
+            if brk ~= nil then
+                brk = uchar(line, WRAP - (brk - 1))
+            else
+                brk = uchar(line, WRAP)
+            end
+            w[#w + 1] = string.sub(line, 1, brk) .. "\n"
+            w[#w + 1] = wrap(string.sub(line, brk+1, -1))
+        end
+    end
+    return table.concat(w)
+end
+
+function t(cond ,T ,F)
+    if cond then return T else return F end
+end
+
 desc = {}
 function get_comments_and_description()
     local path = string.gsub(mp.get_property("path"), "ytdl://", "")
@@ -52,7 +92,13 @@ function get_comments_and_description()
 
     io.output("/tmp/comments.wat")
     for i,v in ipairs(json.comments) do
-        io.write(v.text)
+        local op = "" local fav = "" local indent = ""
+        if v.author_is_uploader then op = " " end
+        if v.is_favorited then fav = " " end
+        if v.parent ~= "root" then indent = "   " end
+        io.write(v.author, op, fav, " • ", v.likes, " Likes", " • ",
+            v.time_text, "\n", COMMENTS_PREFIX,
+            string.gsub(wrap(v.text), "\n", indent..COMMENTS_PREFIX.."\n"), "\n")
     end
 
     --proxychains -q youtube-dl -j --write-comments \
