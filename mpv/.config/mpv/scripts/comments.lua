f2735c7c39fed3b523f379b108ec339c97d57176
Ehsan G <ehsangn@protonmail.ch>
2021-10-27 16:50:08 +0330

    finish comments.lua

--- mpv/.config/mpv/scripts/comments.lua
+++ mpv/.config/mpv/scripts/comments.lua
@@ -1,15 +1,23 @@
+-- get the description and comments of videos.
 -- config
 DESCRIPTION_KEY = 'ctrl+d'
 COMMENTS_KEY = 'ctrl+c'
+MAX_COMMENTS = 200
 COMMENTS_PREFIX = "│"
 WRAPCOL = 75
+CACHEDIR = (os.getenv("XDG_CACHE_HOME") or os.getenv("HOME").."/.cache").."/description"
 ---
 
 local mp = require 'mp'
 local utils = require 'mp.utils'
 
-local shell = {}
-function shell.escape(args)
+local function exec(args)
+    local r = mp.command_native({name = "subprocess", args = args,
+        capture_stdout = true, capture_stderr = true})
+    return r.status == 0, r.stdout
+end
+
+function shexec(args)
     local ret = {}
     for _,a in pairs(args) do
         s = tostring(a)
@@ -18,17 +26,7 @@ function shell.escape(args)
         end
         table.insert(ret,s)
     end
-    return table.concat(ret, " ")
-end
-
-function shell.exec(args)
-    return os.execute(shell.escape(args))
-end
-
-local function exec(args)
-    local r = mp.command_native({name = "subprocess", args = args,
-        capture_stdout = true, capture_stderr = true})
-    return r.status == 0, r.stdout
+    return os.execute(table.concat(ret, " "))
 end
 
 -- separate the given string into lines.
@@ -54,6 +52,7 @@ function wrap(s)
             w[#w + 1] = line .. "\n"
         else
             -- find the position of the first space character before the WRAPCOLth column
+            --local brk = line:sub(1, WRAPCOL):reverse():find("%s")
             local brk = string.find(string.reverse(string.sub(line, 1, uchar(line, WRAPCOL))), "%s")
             if brk ~= nil then
                 brk = uchar(line, WRAPCOL - (brk - 1))
@@ -67,74 +66,67 @@ function wrap(s)
     return table.concat(w)
 end
 
--- -- https://github.com/philanc/plc/blob/master/plc/checksum.lua
--- local function crc32(s)
---     local b, crc, mask
---     crc = 0xffffffff
---     for i = 1, #s do
---         b = string.byte(s, i)
---         crc = crc ~ b
---         for _ = 1, 8 do
---             mask = -(crc & 1)
---             crc = (crc >> 1) ~ (0xedb88320 & mask)
---         end
---     end
---     return (~crc) & 0xffffffff
--- end
-
 function isfile(path)
    local f = io.open(path, "r")
    if f ~= nil then io.close(f) return true else return false end
 end
 
-desc = {}
-function get_comm_desc()
-    local url = string.gsub(mp.get_property("path"), "ytdl://", "")
+function geturl()
+    return string.gsub(mp.get_property("path"), "ytdl://", "")
+end
+
+descfile = {}
+function savedesc()
+    local url = geturl()
     if isfile(url) then return end
 
+    local id = url:gsub(".-%w/", ""):gsub("[^%w%-_]", ""):reverse()
+    descfile[url] = {}
+    descfile[url].desc = CACHEDIR.."/"..id..".desc"
+    descfile[url].comm = CACHEDIR.."/"..id..".comm"
+    if isfile(descfile[url].desc) or isfile(descfile[url].comm) then return end
+
     local status, json = exec{"yt-dlp", "--no-playlist", "-j", "--write-comments",
-        "--extractor-args", "youtube:comment_sort=top;max_comments=100", url}
+        "--extractor-args", "youtube:comment_sort=top;max_comments="..MAX_COMMENTS,
+        "--", url}
     if not status then return end
     local json = utils.parse_json(json)
     if not json then return end
 
-    dir = (os.getenv("XDG_CACHE_HOME") or os.getenv("HOME").."/.cache").."/comments"
-    if not exec{"mkdir", "-pm700", dir} then return end
+    if not exec{"mkdir", "-pm700", CACHEDIR} then return end
 
-    -- local id = crc32(url)
-    local id = json.id
-    comm = dir.."/"..id..".comm"
-    desc = dir.."/"..id..".desc"
-
-    if (not isfile(desc)) and json.description then
-        local f = assert(io.open(desc, "w"))
+    if json.description then
+        local f = assert(io.open(descfile[url].desc, "w"))
         f:write(wrap(json.description))
         f:close()
     end
 
-    if isfile(comm) or (not json.comments) then return end
-    local f = assert(io.open(comm, "w"))
+    if not json.comments then return end
+    local f = assert(io.open(descfile[url].comm, "w"))
     for i,v in ipairs(json.comments) do
-        local op = "" local fav = "" local indent = ""
+        local op = "" local fav = "" local indent = COMMENTS_PREFIX
         if v.author_is_uploader then op = " " end
         if v.is_favorited then fav = " " end
-        if v.parent ~= "root" then indent = "   " end
-        f:write(v.author, op, fav, " • ", v.likes, " Likes", " • ",
-            v.time_text, "\n", COMMENTS_PREFIX,
-            string.gsub(wrap(v.text), "\n", indent..COMMENTS_PREFIX.."\n"), "\n")
+        if v.parent ~= "root" then indent = "   "..COMMENTS_PREFIX end
+        local text = indent..v.author..op..fav.." • "..v.like_count..
+            " Likes".." • "..v.time_text.."\n"..v.text
+        text = wrap(text):gsub("\n$", ""):gsub("\n", "\n"..indent)
+        f:write(text, "\n\n")
     end
     f:close()
 end
 
-function page(file)
-    term = os.getenv("TERMINAL")
+function page(f)
+    local file = descfile[geturl()][f]
+    if not isfile(file) then return end
+    local term = os.getenv("TERMINAL")
     if term then
-        shell.exec{"setsid", "-f", term, "-e", os.getenv("PAGER") or "less", file, ">/dev/null 2>&1"}
+        shexec{"setsid", "-f", term, "-e", os.getenv("PAGER") or "less", file, ">/dev/null 2>&1"}
     else
-        shell.exec{"setsid", "-f", "xdg-open", file, ">/dev/null 2>&1"}
+        shexec{"setsid", "-f", "xdg-open", file, ">/dev/null 2>&1"}
     end
 end
 
-mp.add_forced_key_binding(COMMENTS_KEY, "show_comments", function() page(comm) end)
-mp.add_forced_key_binding(DESCRIPTION_KEY, "show_description", function() page(desc) end)
-mp.register_event("start-file", get_comm_desc)
+mp.add_forced_key_binding(COMMENTS_KEY, "show_comments", function() page("comm") end)
+mp.add_forced_key_binding(DESCRIPTION_KEY, "show_description", function() page("desc") end)
+mp.register_event("start-file", savedesc)
