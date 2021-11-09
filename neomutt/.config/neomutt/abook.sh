cf2ee97c12b34c7ec68902a8ec715874cb21973e
Ehsan G <ehsangn@protonmail.ch>
2021-11-09 11:23:22 +0330

    simplify abook.sh

--- neomutt/.config/neomutt/abook.sh
+++ neomutt/.config/neomutt/abook.sh
@@ -2,19 +2,8 @@
 # run abook with changed config and addressbook paths
 
 # show everything in the alternate buffer
-trap exit INT TERM HUP
-trap "printf '\e[?1049l'" EXIT
-printf '\e[?1049h'
-printf '\e[H'
+printf '\033[?1049h\033[H'
 
 # run abook
-abook \
-    --config ~/.config/neomutt/abook.conf \
-    --datafile ~/.local/share/neomutt/mailbook/addressbook "$@" ||
-    case "$1" in
-        *query*) notify-send NeoMutt 'Failed fetching emails from the addressbook' ;;
-        *add*) notify-send NeoMutt 'Failed adding the email to the addressbook' ;;
-    esac
-
-# send a key to exit out of neomutt's prompt
-xdotool key period
+abook --config ~/.config/neomutt/abook.conf \
+    --datafile ~/.local/share/neomutt/mailbook/addressbook "$@"
