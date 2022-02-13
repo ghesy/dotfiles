8e9125c32544ddf1aef2d1dc75631118eec67b67
Ehsan Ghorbannezhad <ehsan@disroot.org>
2022-02-13 20:41:50 +0330

    description.lua: massive improvements on all fronts
    the most improtant change is that all the info is now put
    inside of one file, and it's bound to ctrl+d.
    
    also we're now fetching likes, dislikes (even for youtube videos), and views.
    
    there are many more improvements here and there too.

--- mpv/.config/mpv/scripts/description.lua
+++ mpv/.config/mpv/scripts/description.lua
@@ -1,17 +1,18 @@
--- fetch and show comments and description of videos.
--- requires yt-dlp (youtube-dl can't fetch comments).
+-- fetch and show video description, comments, likes, dislikes etc.
+-- requires yt-dlp (youtube-dl can't fetch comments) and curl.
 -- author: Ehsan Ghorbannezhad <ehsan@disroot.org>
 
--- config
-DESCRIPTION_KEY = 'ctrl+d' -- binding to open the video description in your pager in a new terminal window.
-COMMENTS_KEY = 'ctrl+c' -- same as above but for the comments.
-TRY_PROXYCHAINS = true -- try to use proxychains for connection if normal connection is not possible.
+----- CONFIG -----
+KEY = 'ctrl+d' -- binding to open the file containing the description, comments etc.
+TRY_PROXYCHAINS = true -- try to use proxychains for connection if normal connection is not possible. requires proxychains.
 MAX_COMMENTS = 10 -- limit the number of comments to fetch.
 MAX_REPLIES = 5 -- limit the number of replies per comment to fetch.
-WRAPCOL = 70 -- wrap lines longer that this many characters.
-COMMENTS_PREFIX = '│'
-CACHEDIR = (os.getenv("XDG_CACHE_HOME") or os.getenv("HOME").."/.cache").."/description"
----
+MAX_WIDTH = 70 -- wrap lines longer that this many characters.
+COMMENT_PREFIX = '│' -- the string appearing before each line of the comments
+SECTION_SEPARATOR = "\n"..string.rep("=", 75).."\n\n" -- the string appearing between each section (description, comments etc.)
+CACHE_DIR = (os.getenv("XDG_CACHE_HOME") or os.getenv("HOME").."/.cache").."/description" -- where to save the files
+DISLIKE_API_BASE = "https://returnyoutubedislikeapi.com/votes?videoId=" -- base URL of the returnyoutubedislike API
+------------------
 
 local mp = require 'mp'
 local utils = require 'mp.utils'
@@ -54,18 +55,23 @@ function isfile(path)
    if f ~= nil then io.close(f) return true else return false end
 end
 
--- get the source url of a downloaded video file from it's metadata
+-- get the source URL of a downloaded video file from it's metadata
 function getfileurl(path)
-    local status, url = exec{"ffprobe", "-loglevel", "error",
+    local success, url = exec{"ffprobe", "-loglevel", "error",
         "-show_entries", "format_tags=purl", "-of", "default=nk=1:nw=1", path}
-    if status == false or url == nil then url = "" end
+    if success == false or url == nil then url = "" end
     return url:gsub("\n", "")
 end
 
--- get the url of the currently playing video
+-- get the URL of the currently playing video
 function geturl()
     local url = string.gsub(mp.get_property("path"), "ytdl://", "")
     if isfile(url) then return getfileurl(url) else return url end
+end
+
+-- test wether the given URL belongs to youtube
+function isyoutube(url)
+    return url:find("://youtu%.be/") ~= nil or url:find("://w*%.?youtube%.com/") ~= nil
 end
 
 -- separate the given string into lines.
@@ -74,19 +80,19 @@ function getlines(s)
     return s:gmatch("(.-)\n")
 end
 
--- wrap lines of the given string to WRAPCOL columns.
+-- wrap lines of the given string to MAX_WIDTH columns.
 function wrap(s)
     local w = {}
     for line in getlines(s) do
-        if #line <= WRAPCOL then
+        if #line <= MAX_WIDTH then
             w[#w + 1] = line .. "\n"
         else
-            -- find the position of the first space character before the WRAPCOLth column
-            local brk = line:sub(1, WRAPCOL):reverse():find("%s")
+            -- find the position of the first space character before the MAX_WIDTHth column
+            local brk = line:sub(1, MAX_WIDTH):reverse():find("%s")
             if brk ~= nil then
-                brk = WRAPCOL - (brk - 1)
+                brk = MAX_WIDTH - (brk - 1)
             else
-                brk = WRAPCOL
+                brk = MAX_WIDTH
             end
             w[#w + 1] = string.sub(line, 1, brk) .. "\n"
             w[#w + 1] = wrap(string.sub(line, brk+1, -1))
@@ -95,9 +101,39 @@ function wrap(s)
     return table.concat(w)
 end
 
+-- shorten and format the given number (eg. 4560 -> 4.5K)
+function numshorten(n)
+    if n >= 10^6 then
+        return string.format("%.1fM", n/10^6)
+    elseif n >= 10^3 then
+        return string.format("%.1fK", n/10^3)
+    else
+        return tostring(n)
+    end
+end
+
+-- round the given number
+function round(n)
+    return math.floor(n+0.5)
+end
+
+-- generate a nice unicode progress bar
+function progressbar(percent)
+    local step = 5
+    local phases = {' ', '▏', '▎', '▍', '▌', '▋', '▊', '▉', '█'}
+    local nwhole = math.floor(percent / step)
+    local nphase = round(percent % step / (step / (#phases - 1)))
+    local nempty = (100 / step) - (nwhole + 1)
+    local phasechar = phases[nphase+1]
+    if percent == 100 then phasechar = '' end
+
+    return ">" .. string.rep(phases[#phases], nwhole) .. phasechar ..
+        string.rep(' ', nempty) .. "<"
+end
+
 -- open description or comments in a pager in a terminal window
-function pager(f)
-    local file = descfile[geturl()][f]
+function opendesc()
+    local file = descfile[geturl()]
     if not isfile(file) then return end
     local term = os.getenv("TERMINAL")
     if term then
@@ -107,63 +143,115 @@ function pager(f)
     end
 end
 
--- save the description and comments fetched by fetchdesc() to CACHEDIR
+-- save the description and comments fetched by fetchdesc() to CACHE_DIR
 function savedesc(success, result, error)
     if (not success) or (result.status ~= 0) then return end
     local json = utils.parse_json(result.stdout)
     if not json then return end
-    if not exec{"mkdir", "-pm700", CACHEDIR} then return end
+    if not exec{"mkdir", "-pm700", CACHE_DIR} then return end
     local url = geturl()
 
-    -- format and write the description to cache
+    -- if no important info is available, do not proceed
+    if (not json.description) and (not json.comments) and
+       (not json.like_count)  and (not json.dislike_count) then
+        return
+    end
+
+    -- open the file for writing
+    local f = assert(io.open(descfile[url], "w"))
+
+    -- write extractor info
+    if json.extractor or json.webpage_url_domain then
+        f:write("Source: ")
+        if json.extractor then f:write(json.extractor) end
+        if json.extractor_key then f:write(" - ", json.extractor_key) end
+        if json.webpage_url_domain then f:write(" - ", json.webpage_url_domain) end
+        f:write("\n", SECTION_SEPARATOR)
+    end
+
+    -- write title, channel name, like and view count etc.
+    if json.title      then f:write("Title: ",   json.title,   "\n") end
+    if json.channel    then f:write("Channel: ", json.channel, "\n") end
+    if json.view_count then f:write(numshorten(json.view_count), " views\n") end
+
+    -- if it's a youtube video, get the dislike count from returnyoutubedislike's API
+    if not json.dislike_count and isyoutube(url) then
+        local success, stdout = exec{"curl", "-fsL", "--", DISLIKE_API_BASE..json.id}
+        if success == true and stdout ~= "" then
+            json.dislike_count = utils.parse_json(stdout).dislikes
+        end
+    end
+
+    -- write likes and dislikes
+    if json.like_count and json.dislike_count then f:write("\n ") end
+    if json.like_count or  json.dislike_count then
+        local nlike = "-"
+        local ndislike = "-"
+        if json.like_count    then nlike    = numshorten(json.like_count)    end
+        if json.dislike_count then ndislike = numshorten(json.dislike_count) end
+        f:write(string.format("%-9s    %9s\n", "﨓"..nlike, "﨑"..ndislike))
+    end
+    if json.like_count and json.dislike_count then
+        local percent = json.like_count / (json.like_count + json.dislike_count) * 100
+        percent = string.format("%.2f", percent)
+        f:write("        ", percent, "%\n", progressbar(percent), "\n")
+    end
+
+    f:write(SECTION_SEPARATOR)
+
+    -- write description
     if json.description then
-        local f = assert(io.open(descfile[url].desc, "w"))
-        f:write(wrap(json.description))
-        f:close()
+        f:write(wrap(json.description), SECTION_SEPARATOR)
     end
 
     -- format and write the comments to cache
-    if not json.comments then return end
-    local f = assert(io.open(descfile[url].comm, "w"))
-    for i,v in ipairs(json.comments) do
-        local op = "" local fav = "" local indent = COMMENTS_PREFIX
-        if v.parent ~= "root" then indent = "   "..COMMENTS_PREFIX end
-        if v.author_is_uploader then op = " " end
-        if v.is_favorited then fav = " " end
-        local text = indent..v.author..op..fav.." • "..v.like_count..
-            " Likes".." • "..v.time_text.."\n"..v.text
-        text = wrap(text):gsub("\n$", ""):gsub("\n", "\n"..indent)
-        f:write(text, "\n\n\n")
+    if json.comments then
+        for i,v in ipairs(json.comments) do
+            local op = "" local fav = "" local indent = COMMENT_PREFIX
+            if v.parent ~= "root" then indent = "   "..COMMENT_PREFIX end
+            if v.author_is_uploader then op = " " end
+            if v.is_favorited then fav = " " end
+            local text = indent .. v.author .. op .. fav .. "   " .. "﨓" ..
+                numshorten(v.like_count) .. "   "..v.time_text.."\n"..v.text
+            text = wrap(text):gsub("\n$", ""):gsub("\n", "\n"..indent)
+            f:write(text, "\n\n\n")
+        end
     end
     f:close()
 end
 
+-- generate a unique ID for the given URL
+function genid(url)
+    local id = url:gsub("^.-[^:/]/", "")
+    if isyoutube(url) then
+        id = id:gsub("^watch%?v=", "")
+    end
+    return id:gsub("[^%w%-_]", "")
+end
+
 -- download description and comments and hand it to savedesc()
 function fetchdesc()
-    -- return if no url is associated with the video
+    -- return if no URL is associated with the video
     local url = geturl()
     if url == nil or url == "" then return end
 
     -- set the path of cache files
-    local id = url:gsub(".-%w/", ""):gsub("[^%w%-_]", ""):reverse()
-    descfile[url] = {}
-    descfile[url].desc = CACHEDIR.."/"..id..".desc"
-    descfile[url].comm = CACHEDIR.."/"..id..".comm"
-    if isfile(descfile[url].desc) or isfile(descfile[url].comm) then return end
+    local id = genid(url)
+    descfile[url] = CACHE_DIR.."/"..id..".txt"
+    if isfile(descfile[url]) then return end
 
     -- get the video's infojson using yt-dlp and send it's result to formatdesc()
     local args = {"yt-dlp", "--no-playlist", "-j", "--write-comments", "--extractor-args",
         "youtube:comment_sort=top;max_comments=all,"..MAX_COMMENTS..",all,"..MAX_REPLIES, "--", url}
 
     if TRY_PROXYCHAINS then
-        local status, stdout = exec{"curl", "-sLIm8", "--", url}
-        if status == false or stdout == "" then
+        local success, stdout = exec{"curl", "-sLIm8", "--", url}
+        if success == false or stdout == "" then
             args = {"proxychains", "-q", table.unpack(args)}
         end
     end
     execasync(savedesc, args)
 end
 
-mp.add_forced_key_binding(DESCRIPTION_KEY, "show_description", function() pager("desc") end)
-mp.add_forced_key_binding(COMMENTS_KEY, "show_comments", function() pager("comm") end)
+mp.add_forced_key_binding(KEY, "show_description", function() opendesc() end)
 mp.register_event("start-file", fetchdesc)
