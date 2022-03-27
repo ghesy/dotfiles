7858d90da642d06dced7ca97a3b2d2b88dfc49c6
Ehsan Ghorbannezhad <ehsan@disroot.org>
2022-03-27 17:22:28 +0430

    remove unneeded symbols in some scripts

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ cd "$(dirname "$(readlink -f "${0:?}")")" || exit 1
 ! command -v stow >/dev/null && echo Please install stow. && exit 1
 
 # make these directories so that stow can't create them as symlinks
-l=~/.local; s=$l/share c=~/.config
+l=~/.local s=$l/share c=~/.config
 mkdir -p $l/bin $l/sv/run $s/applications $s/gnupg $c/kicad \
     $c/pulse $c/gtk-2.0 $c/gtk-3.0 $c/transmission-daemon \
     $s/local/profile.d $s/local/xinitrc.d $s/local/gimme/aliases.d
--- sysfiles/.local/share/sysfiles/dns-caching/update-resolv.sh
+++ sysfiles/.local/share/sysfiles/dns-caching/update-resolv.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # /etc/windscribe/update-resolv.sh
-split_into_parts() { part1="$1"; part2="$2"; part3="$3" ;}
+split_into_parts() { part1=$1 part2=$2 part3=$3 ;}
 case "$script_type" in
     up)
         for optionvarname in ${!foreign_option_*} ; do
-            option="${!optionvarname}"; echo "$option"
+            option=${!optionvarname}; echo "$option"
             split_into_parts $option
             [ "$part1" = "dhcp-option" ] && [ "$part2" = "DNS" ] &&
                 echo "nameserver $part3" | resolvconf -x -a windscribe
