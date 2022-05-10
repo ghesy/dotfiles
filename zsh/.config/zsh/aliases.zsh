690847b5adf12cf93877c3f447026d0a673d6ad2
Ehsan Ghorbannezhad <ehsan@disroot.org>
2022-05-11 02:47:21 +0430

    fix some zsh functions and add some aliases

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -2,23 +2,26 @@
 
 # remove the called command from the terminal's output
 rmcmd() {
-    repeat ${#${(f)PS1}} { echo -ne '\e[A\e[2K' }
+    repeat "${#${(f)PS1}}" { echo -ne '\e[A\e[2K' }
 }
 
 # search the filesystem using fzf
 s() {
-    rmcmd
-    cd "$(finder "$@")"
+    #rmcmd
+    local p
+    p=$(finder) || return 1
+    [[ -f $p ]] && p=${p:h}
+    [[ ${p:A} != ${PWD:A} ]] && cd "$p"
 }
 
 # search frequent files using fzf
 f() {
-    rmcmd
+    #rmcmd
     local p
     p=$(freq -m fzf) || return 1
     p=${p/#'~'/"$HOME"}
-    [[ -f $p ]] && p=${p%/*}
-    cd "$p"
+    [[ -f $p ]] && p=${p:h}
+    [[ ${p:A} != ${PWD:A} ]] && cd "$p"
 }
 
 # show the help message of the given program in the pager
@@ -33,19 +36,21 @@ paru() {
 }
 
 lf() {
-    rmcmd
+    #rmcmd
     [[ -n $LF_LEVEL ]] && exit
     local tmp
     tmp=$(mktemp) || return
     command lf -last-dir-path="$tmp" "$@"
     local dir=$(<"$tmp")
     rm "$tmp"
-    [[ -d $dir ]] && cd "$dir"
+    [[ ${dir:A} != ${PWD:A} ]] && [[ -d $dir ]] && cd "$dir"
 }
 
 # basic stuff
 alias a='command ls -AFt --color=always --group-directories-first'
 alias A='a -l'
+alias l='a'
+alias L='A'
 alias t='tree -laFtrC --dirsfirst -L 2'
 alias 1='t -L 1 -i --noreport'
 alias md='mkdir -pv'
@@ -53,7 +58,8 @@ alias cp='advcp -ig'
 alias mv='advmv -ig'
 alias T='trash-put'
 alias rm='rm -Iv --one-file-system'
-alias v=nvim
+alias v='nvim'
+alias vi='nvim'
 
 # pacman
 alias p='paru'
