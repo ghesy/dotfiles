535f2556e1367a3fff421f6bdd0f0d58688360d1
Ehsan Ghorbannezhad <ehsan@disroot.org>
2022-05-26 21:50:26 +0430

    new mpv script to set the fonts properly

---
+++ mpv/.config/mpv/scripts/setfonts.lua
@@ -0,0 +1,17 @@
+-- author: Ehsan Ghorbannezhad <ehsan@disroot.org>
+-- properly set mpv's subtitle and osd fonts to the system's sans-serif font
+-- mpv is supposed to set the --sub-font and --osd-font options to sans-serif
+-- by default, but for some reason libass defaults to NotoSans or whatever.
+
+local r = mp.command_native{
+	name = "subprocess",
+    capture_stdout = true,
+	capture_stderr = true,
+	playback_only = false,
+	args = { "fc-match", "-f%{family}", "sans-serif" },
+}
+
+if r and r.status == 0 then
+	mp.set_property("osd-font", r.stdout)
+	mp.set_property("sub-font", r.stdout)
+end
