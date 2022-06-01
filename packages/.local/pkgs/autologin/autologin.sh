f879439c39157fff7efb61ae5c801458186edcc6
Ehsan Ghorbannezhad <ehsan@disroot.org>
2022-06-01 20:55:04 +0430

    fix and improve the 'autologin' package

--- packages/.local/pkgs/autologin/autologin.sh
+++ packages/.local/pkgs/autologin/autologin.sh
@@ -1,4 +1,19 @@
 #!/bin/sh
 # /usr/share/libalpm/scripts/autologin
-sed -i 's|--noclear|--noclear --autologin koonix|' /run/runit/service/agetty-tty1/conf
+
+# config
+user=koonix
+tty=1
+
+# disable elogind being started through dbus
 sed -i 's|^Exec=.*|Exec=/bin/false|' /usr/share/dbus-1/system-services/org.freedesktop.login1.service
+
+# service directory
+sv=/run/runit/service/agetty-tty${tty:?}
+
+# configure agetty to autologin
+sed -Ei 's|(\S*GETTY_ARGS=).*|\1"--noclear --autologin '"${user:?}"'"|' ${sv:?}/conf
+
+# make agetty's service wait for elogind
+grep -q 'sv check elogind' ${sv:?}/run ||
+    sed -i '2i sv check elogind >/dev/null || exit 1' ${sv:?}/run
