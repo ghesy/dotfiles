5239a4cc68b3db49a9b4a1e4e8379465a5ae835d
Ehsan Ghorbannezhad <ehsan@disroot.org>
2022-06-02 20:22:05 +0430

    new package: loginctl-chvt

---
+++ packages/.local/pkgs/loginctl-chvt/loginctl-chvt.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+# a wrapper for loginctl that switches to tty6 before suspend.
+# an elogind hook will then switch back to the previous tty
+# after waking up from suspend.
+
+# config
+sparetty=6
+origtty=/run/origtty
+
+set -e
+
+[ $# -eq 0 ] && exec /usr/bin/loginctl
+
+# the --chvt option is for internal use
+if [ "$1" = --chvt ]; then
+    shift
+    /usr/bin/loginctl lock-session
+    sleep 0.5
+    fgconsole > "$origtty"
+    chvt "${sparetty:?}"
+    exec /usr/bin/loginctl "$@"
+fi
+
+for arg; do
+    case $arg in
+        -*) continue ;;
+        suspend|hibernate|hybrid-sleep|suspend-then-hibernate)
+            exec sudo -- "$0" --chvt "$@"
+        ;;
+        *) break ;;
+    esac
+done
+
+exec /usr/bin/loginctl "$@"
