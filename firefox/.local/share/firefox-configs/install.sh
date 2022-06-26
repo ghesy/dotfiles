edbae063a94c485e120d6b544601963210ef9bcf
Ehsan Ghorbannezhad <ehsan@disroot.org>
2022-06-26 21:46:16 +0430

    add a customized handlers.json to firefox configs

--- firefox/.local/share/firefox-configs/install.sh
+++ firefox/.local/share/firefox-configs/install.sh
@@ -2,4 +2,5 @@
 for profile_dir in ~/.mozilla/firefox/????????.*/; do
     install -vbCDm644 user.js -t "${profile_dir%/}"/
     install -vbCDm644 userChrome.css -t "${profile_dir%/}"/chrome
+    install -vbCDm644 handlers.json -t "${profile_dir%/}"/
 done
