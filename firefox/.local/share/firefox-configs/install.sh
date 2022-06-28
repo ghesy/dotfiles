5b0996f2c22adfc2710368ff58b0a481249c73d3
Ehsan Ghorbannezhad <ehsan@disroot.org>
2022-06-28 14:37:39 +0430

    simplify firefox-configs's install.sh

--- firefox/.local/share/firefox-configs/install.sh
+++ firefox/.local/share/firefox-configs/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 for profile_dir in ~/.mozilla/firefox/????????.*/; do
-    install -vbCDm644 user.js -t "${profile_dir%/}"/
-    install -vbCDm644 userChrome.css -t "${profile_dir%/}"/chrome
-    install -vbCDm644 handlers.json -t "${profile_dir%/}"/
+    install -vbCDm644 user.js -t "$profile_dir/"
+    install -vbCDm644 userChrome.css -t "$profile_dir/chrome/"
+    install -vbCDm644 handlers.json -t "$profile_dir/"
 done
